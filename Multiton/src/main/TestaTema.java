f07b6740a82cb4a9db6108e6072780ab73385a7a
DanMke <danmke@hotmail.com>
2019-06-26 14:05:24 -0300

    Added Multiton
    Signed-off-by: DanMke <danmke@hotmail.com>

---
+++ Multiton/src/main/TestaTema.java
@@ -0,0 +1,21 @@
+package main;
+
+import multiton.Tema;
+
+public class TestaTema {
+
+	public static void main(String[] args) {
+		
+		Tema temaFire = Tema.getInstance(Tema.FIRE);
+		System.out.println(" Tema " + temaFire.getNome());
+		System.out.println(" Cor Da Fonte : " + temaFire.getCorDaFonte());
+		System.out.println(" Cor Do Fundo : " + temaFire.getCorDoFundo());
+		
+		Tema temaFire2 = Tema.getInstance(Tema.FIRE);
+		System.out.println(" --------");
+		System.out.println(" Comparando as referências ... ");
+		System.out.println(temaFire == temaFire2);
+
+	}
+
+}
